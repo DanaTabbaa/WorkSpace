c5bf2adfc35cde6f959e553dad20591285e12726
tabaa200@gmail <tabaa200@gmail>
2024-03-23 13:42:12 +0300

    first_commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
+
+import { FullLayoutComponent } from './layouts/full-layout/full-layout.component';
+import { CommonLayoutComponent } from './layouts/common-layout/common-layout.component';
+
+import { FullLayout_ROUTES } from './shared/routes/full-layout.routes';
+import { CommonLayout_ROUTES } from './shared/routes/common-layout.routes';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard/home',
+    pathMatch: 'full',
+  },
+
+  {
+    path: '',
+    component: FullLayoutComponent,
+    children: FullLayout_ROUTES,
+  },
+  {
+    path: '',
+    component: CommonLayoutComponent,
+    children: CommonLayout_ROUTES,
+  },
+  //authentication routes.
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes, {
+      preloadingStrategy: PreloadAllModules,
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
